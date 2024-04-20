d264bda09e62887af3d27b29ef009c618747fa07
Samihan <ssmuppirala@gmail.com>
2024-04-20 00:58:23 -0700

    Added window title + size

--- src/gui.py
+++ src/gui.py
@@ -6,6 +6,8 @@ from tk_components.components import MainApplication
 
 def main():
     root = tk.Tk()
+    root.title("CS:GO Demo Visualizer")
+    root.geometry("1920x1080")
     MainApplication(root).pack(side='top', fill='both', expand=True)
     # Exit the application when the window is closed
     # This is required because, for some reason, the initial instantiation of the FigureCanvasTkAgg prevents the script from terminating even after the window is closed.
