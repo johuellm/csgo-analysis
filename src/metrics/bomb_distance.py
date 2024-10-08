87659f2528d02e293e77bd5fd84fa9df8aebff28
Joschka Hüllmann <joschka.huellmann@uni-muenster.de>
2024-10-08 18:36:34 +0200

    finished first basic bomb distance metric

--- src/metrics/bomb_distance.py
+++ src/metrics/bomb_distance.py
@@ -1,52 +1,82 @@
 import matplotlib.pyplot as plt
 
-from awpy.analytics.nav import area_distance
+from awpy.analytics.nav import area_distance, find_closest_area
 from awpy.data import NAV
 from awpy.visualization.plot import plot_map, position_transform
 from matplotlib import patches
 
 from models.data_manager import DataManager
 
+## TODO: Find onlyy bombsite area tiles where you can plant
+## TODO: Use distance lookup table to speed things up
 
-def process_bomb_distance(dm: DataManager, round_idx: int, frame_idx: int, plot: bool = False):
-  print("Calculating metrics for round %d" % round_idx)
-  testframe = dm.get_frame(round_idx, frame_idx)
+def process_bomb_distance_frame(dm: DataManager, round_idx: int, frame_idx: int, plot_distance: bool = False):
+  print("Calculating bomb distance metrics for round %d, frame %d" % (round_idx, frame_idx))
   map_name = dm.get_map_name()
 
   if map_name not in NAV:
     raise ValueError("Map not found.")
 
-
   # find shorted bombsite:
-  closest_bombsite_area = float("Inf")
-
+  closest_bombsite_dist = float("Inf")
   bombinfo = dm.get_bomb_info(round_idx, frame_idx)
-  ## todo: find closest area from bomb position
+  bomb_coords = [bombinfo[key] for key in bombinfo]
+  area_bomb = find_closest_area(map_name, point=bomb_coords, flat=False)
 
   ## todo: find bombsite area with minimum distance from bomb
   for a in NAV[map_name]:
+    area = NAV[map_name][a]
     if area["areaName"].startswith("Bombsite"):
+      geodesic_dist = area_distance(map_name=map_name, area_a=a, area_b=area_bomb["areaId"], dist_type="geodesic")
+      if geodesic_dist["distance"] < closest_bombsite_dist:
+        closest_bombsite_dist = geodesic_dist["distance"]
 
+  if not plot_distance:
+    return closest_bombsite_dist
 
-  geodesic_dist = area_distance(map_name=map_name, area_a=340, area_b=8773, dist_type="geodesic")
-
-  ## todo: plot path from bomb to closest bombsite
   fig, ax = plot_map(map_name=map_name, map_type='simpleradar', dark=True)
 
   for a in NAV[map_name]:
     area = NAV[map_name][a]
+    southEastX = position_transform(map_name, area["southEastX"], "x")
+    northWestX = position_transform(map_name, area["northWestX"], "x")
+    southEastY = position_transform(map_name, area["southEastY"], "y")
+    northWestY = position_transform(map_name, area["northWestY"], "y")
+    width = (southEastX - northWestX)
+    height = (northWestY - southEastY)
+
     color = "None"
     if a in geodesic_dist["areas"]:
       color = "red"
-    width = (area["southEastX"] - area["northWestX"])
-    height = (area["northWestY"] - area["southEastY"])
-    southwest_x = area["northWestX"]
-    southwest_y = area["southEastY"]
-    rect = patches.Rectangle((southwest_x, southwest_y), width, height, linewidth=1, edgecolor="yellow",
+
+    rect = patches.Rectangle((northWestX, southEastY), width, height, linewidth=1, edgecolor="yellow",
                              facecolor=color)
     ax.add_patch(rect)
 
   fig.show()
+  return closest_bombsite_dist
+
+
+
+def process_bomb_distance_round(dm: DataManager, round_idx: int):
+  print("Calculating bomb distance metrics for round %d" % round_idx)
+  map_name = dm.get_map_name()
+
+  if map_name not in NAV:
+    raise ValueError("Map not found.")
+
+  bomb_distance_metrics: list[float] = []
+  for frame_idx, frame in enumerate(dm.get_game_round(round_idx)["frames"] or []) :
+    frame_bomb_distance = process_bomb_distance_frame(dm, round_idx, frame_idx)
+    bomb_distance_metrics.append(frame_bomb_distance)
+
+  # plot  (for debug)
+  import matplotlib.pyplot as plt
+  import numpy as np
+  plt.scatter(np.arange(len(bomb_distance_metrics)), bomb_distance_metrics)
+  plt.show()
+
+  return bomb_distance_metrics
 
 
 
@@ -68,11 +98,11 @@ def _util_find_bomb_area_ids(map_name: str):
     col = "None"
     if area["areaName"].startswith("Bombsite"):
       col = "red"
-    rect = patches.Rectangle((northWestX, southEastY*10), width*10, height*10, linewidth=1, edgecolor="yellow",
+    rect = patches.Rectangle((northWestX, southEastY), width, height, linewidth=1, edgecolor="yellow",
                              facecolor=col)
     ax.add_patch(rect)
     if area["areaName"].startswith("Bombsite"):
-      ax.text(northWestX*10, northWestY*10, str(a), fontsize=4, color="white")
+      ax.text(northWestX, northWestY, str(a), fontsize=4, color="white")
   f.show()
 
 
@@ -87,4 +117,4 @@ if __name__ == "__main__":
   # map_control_fig, map_control_axes = plot_frame_map_control(dm.get_map_name(), testframe, plot_type='players')
   # map_control_fig.show()
 
-  process_bomb_distance(dm, 5, 8, True)
+  process_bomb_distance_frame(dm, 5, 8, True)
