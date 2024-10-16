5e8ce97d59a09e75580e5a62e0f26b9b4a0becdd
Joschka Hüllmann <joschka.huellmann@uni-muenster.de>
2024-10-16 19:57:10 +0200

    added hp, damage, and axis labels to Rmarkdown

--- src/R/analysis-bombdistance.R
+++ src/R/analysis-bombdistance.R
@@ -16,7 +16,7 @@ setwd("..\\src\\")
 df <- read.csv("testdemo6.csv")
 df$afterAdverseEvent = df$t_alivePlayers < 5
 df$bombPlanted = as.logical(df$bombPlanted)
-
+df$tDmg = 500 - df$ctHp
 
 
 #### DESCRIPTIVE ANALYSIS ####
@@ -36,25 +36,33 @@ plot(c(df.test$t1_x,df.test$t2_x,df.test$t3_x,df.test$t4_x,df.test$t5_x), c(df.t
 
 # plot bombdistance, mapcontrol, hp, playeralive, velocityDeviation
 df.test <- df %>% filter(roundNum == 3) %>%
-	select(velocityDeviation,tHp,ctHp,bombDistance,mapControl,deltaDistance,totalDistance,secondsCalculated,afterAdverseEvent,bombPlanted,bombSeconds)
+	select( velocityDeviation,tHp,tDmg,ctHp,bombDistance,mapControl,deltaDistance,totalDistance,
+			secondsCalculated,afterAdverseEvent,bombPlanted,bombSeconds,
+			t1_x,t2_x,t3_x,t4_x,t5_x,t1_y,t2_y,t3_y,t4_y,t5_y)
 timeAdverseEvent <- df %>% filter(roundNum == 3, afterAdverseEvent == TRUE) %>% pull(secondsCalculated) %>% first()
-par(mfrow=c(2,3))
-plot(df.test$secondsCalculated, df.test$tHp, ylim=c(0,500), col = rgb(df.test$afterAdverseEvent,0,df.test$bombPlanted))
+par(mfrow=c(2,4))
+plot(df.test$secondsCalculated, df.test$tHp, ylim=c(0,500), ylab = "Team T HP", xlab = "Seconds", col = rgb(df.test$afterAdverseEvent,0,df.test$bombPlanted))
 abline(v = df.test$bombSeconds, lty="dashed")
 abline(v = timeAdverseEvent)
-plot(df.test$secondsCalculated, df.test$mapControl, ylim=c(-1,1), col = rgb(df.test$afterAdverseEvent,0,df.test$bombPlanted))
+plot(df.test$secondsCalculated, df.test$tDmg, ylim=c(0,500), ylab = "Team T Damage", xlab = "Seconds", col = rgb(df.test$afterAdverseEvent,0,df.test$bombPlanted))
 abline(v = df.test$bombSeconds, lty="dashed")
 abline(v = timeAdverseEvent)
-plot(df.test$secondsCalculated, df.test$bombDistance, col = rgb(df.test$afterAdverseEvent,0,df.test$bombPlanted))
+plot(c(df.test$t1_x,df.test$t2_x,df.test$t3_x,df.test$t4_x,df.test$t5_x), c(df.test$t1_y,df.test$t2_y,df.test$t3_y,df.test$t4_y,df.test$t5_y),
+	xlim = c(-2200,1800), ylim=c(-1200,3200), ylab = "Y Positions", xlab = "X Positions", col = rgb(df.test$afterAdverseEvent,0,df.test$bombPlanted))
+# no ablines
+plot(df.test$secondsCalculated, df.test$deltaDistance, ylim=c(0,1000), ylab = "Team T Delta Distance", xlab = "Seconds", col = rgb(df.test$afterAdverseEvent,0,df.test$bombPlanted))
 abline(v = df.test$bombSeconds, lty="dashed")
 abline(v = timeAdverseEvent)
-plot(df.test$secondsCalculated, df.test$deltaDistance, col = rgb(df.test$afterAdverseEvent,0,df.test$bombPlanted))
+plot(df.test$secondsCalculated, df.test$totalDistance, ylim=c(0,60000), ylab = "Team T Total Distance", xlab = "Seconds", col = rgb(df.test$afterAdverseEvent,0,df.test$bombPlanted))
 abline(v = df.test$bombSeconds, lty="dashed")
 abline(v = timeAdverseEvent)
-plot(df.test$secondsCalculated, df.test$totalDistance, col = rgb(df.test$afterAdverseEvent,0,df.test$bombPlanted))
+plot(df.test$secondsCalculated, df.test$mapControl, ylim=c(-1,1), ylab = "Map Control", xlab = "Seconds", col = rgb(df.test$afterAdverseEvent,0,df.test$bombPlanted))
 abline(v = df.test$bombSeconds, lty="dashed")
 abline(v = timeAdverseEvent)
-plot(df.test$secondsCalculated, df.test$velocityDeviation, col = rgb(df.test$afterAdverseEvent,0,df.test$bombPlanted))
+plot(df.test$secondsCalculated, df.test$bombDistance, ylim=c(0,4800), ylab = "Distance Bomb to Bombsite", xlab = "Seconds", col = rgb(df.test$afterAdverseEvent,0,df.test$bombPlanted))
+abline(v = df.test$bombSeconds, lty="dashed")
+abline(v = timeAdverseEvent)
+plot(df.test$secondsCalculated, df.test$velocityDeviation, ylim=c(0,210), ylab = "Velocity Deviation", xlab = "Seconds", col = rgb(df.test$afterAdverseEvent,0,df.test$bombPlanted))
 abline(v = df.test$bombSeconds, lty="dashed")
 abline(v = timeAdverseEvent)
 par(mfrow=c(1,1))
@@ -65,6 +73,26 @@ par(mfrow=c(1,1))
 
 # Test that estimated times are correct
 df.test <- df %>% filter(roundNum == 5) %>% mutate(secondsDiff = secondsCalculated - seconds) %>% select(tick, seconds, secondsCalculated, secondsDiff)
+
+# Get map boundaries and other metric boundaries
+# [1] -2186 -> -2200
+df.test <- df
+min(c(df.test$t1_x,df.test$t2_x,df.test$t3_x,df.test$t4_x,df.test$t5_x,df.test$ct1_x,df.test$ct2_x,df.test$ct3_x,df.test$ct4_x,df.test$ct5_x))
+# [1] 1788 -> 1800
+max(c(df.test$t1_x,df.test$t2_x,df.test$t3_x,df.test$t4_x,df.test$t5_x,df.test$ct1_x,df.test$ct2_x,df.test$ct3_x,df.test$ct4_x,df.test$ct5_x))
+# [1] -1163.997 -> -1200
+min(c(df.test$t1_y,df.test$t2_y,df.test$t3_y,df.test$t4_y,df.test$t5_y,df.test$ct1_y,df.test$ct2_y,df.test$ct3_y,df.test$ct4_y,df.test$ct5_y))
+# [1] 3118 -> 3200
+max(c(df.test$t1_y,df.test$t2_y,df.test$t3_y,df.test$t4_y,df.test$t5_y,df.test$ct1_y,df.test$ct2_y,df.test$ct3_y,df.test$ct4_y,df.test$ct5_y))
+# [1] 58568.03 -> 60000
+max(df.test$totalDistance)
+# [1] 937.8394 -> 1000
+max(df.test$deltaDistance)
+# [1] 203.9484 -> 210
+max(df.test$velocityDeviation)
+# [1] 4575.033 -> 4800
+max(df$bombDistance, na.rm = TRUE)
+
 
 
 
